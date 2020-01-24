57af72d86d6a9ac275ed3f3b06d1ea3a6f715600
Ethan <tiandi5001@gmail.com>
2020-01-23 20:01:56 -0500

    Updates

---
+++ precache-manifest.78059c3c19c5e311b50777592c132a21.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5960c7816dc83103dad8dabb20af909",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "69b5ebebe4eb0e3477fa",
+    "url": "/portfolio/static/css/2.09ae8418.chunk.css"
+  },
+  {
+    "revision": "36c69b10d2ce6a7c2484",
+    "url": "/portfolio/static/css/main.4b5f2a2e.chunk.css"
+  },
+  {
+    "revision": "69b5ebebe4eb0e3477fa",
+    "url": "/portfolio/static/js/2.573ae1fc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.573ae1fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36c69b10d2ce6a7c2484",
+    "url": "/portfolio/static/js/main.857eb6b5.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/portfolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/portfolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/portfolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "c037dbbc0e6790f30e824a50010df5fb",
+    "url": "/portfolio/static/media/ionicons.c037dbbc.svg"
+  }
+]);
